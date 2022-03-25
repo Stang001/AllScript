83e0015ab6964f50c85c6ba7b4d884f1f4bdc146
Stang001 <90966008+Stang001@users.noreply.github.com>
2022-03-26 04:12:13 +0700

    Update and rename README.md to FreeMain.lua

---
+++ FreeMain.lua
@@ -0,0 +1,11 @@
+local lp = game.Players.LocalPlayer
+local id = game.PlaceId
+if id == 2753915549 or id == 4442272183 or id == 7449423635 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Stang001/DogeXBF/main/FreeMain.lua"))()
+  elseif id == 7800644383 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Stang001/Untitled-Hood-DogeX/main/FreeMain.lua"))()
+  elseif id == 537413528 then
+  lp:Kick("สคริปต์พัง รออัพเดตหน้า\n Script is Broken Wait for the Next Update")
+  else
+  lp:Kick("สคริปต์แมพนี้ไม่มี ถ้ารันแมพนี้อีกกูขอให้ตูดบาน หำหด\nถ้าอยากให้ทำก็ทักดิสมา")
+  end
