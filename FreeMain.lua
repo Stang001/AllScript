ed00781c80ffb1448b85f4051476b83a3eabd36a
Stang001 <90966008+Stang001@users.noreply.github.com>
2022-04-04 01:42:09 +0700

    Update FreeMain.lua

--- FreeMain.lua
+++ FreeMain.lua
@@ -1,11 +1,18 @@
 local lp = game.Players.LocalPlayer
+local gh = "github.com/Stang001"
+local msg = "Script is Broken Wait for the next update"
 local id = game.PlaceId
 if id == 2753915549 or id == 4442272183 or id == 7449423635 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Stang001/DogeXBF/main/FreeMain.lua"))()
+lp:Kick(msg)
   elseif id == 7800644383 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Stang001/Untitled-Hood-DogeX/main/FreeMain.lua"))()
   elseif id == 537413528 then
-  lp:Kick("สคริปต์พัง รออัพเดตหน้า\n Script is Broken Wait for the Next Update")
+  lp:Kick("msg")
+  elseif id == 5602055394 then
+  _G.Toggle = Enum.KeyCode.V
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Stang001/HoodModdedVersion2/main/DogeX.lua"))()
   else
-  lp:Kick("สคริปต์แมพนี้ไม่มี ถ้ารันแมพนี้อีกกูขอให้ตูดบาน หำหด\nถ้าอยากให้ทำก็ทักดิสมา")
+  lp:Kick("สคริปต์แมพนี้ไม่มี ถ้ารันแมพนี้อีกกูขอให้ตูดบาน หำหด\nถ้าอยากให้ทำก็เม้นมาใน github")
+  wait()
+  setclipboard(gh)
   end
